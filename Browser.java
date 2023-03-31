5c596874ec15b09ac71f43ec9d23111fc7809904
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-31 20:56:29 +0530

    Add files via upload
    Browser.java

---
+++ Browser.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		
+		Chrome Browser= new Chrome();
+		Browser.getName();
+		Browser.PrintName();
+		
+     System.out.println("This is my Browser");
+    
+	}
+
+	}
+

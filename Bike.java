8d7e27b484e2fa6df63307beae19970aec737b9e
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-31 20:53:04 +0530

    Add files via upload
    Bike.java

---
+++ Bike.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("applyBreak");
+		System.out.println("soundHorn");
+		
+		Car bike= new Car();
+		bike.applyBreak();
+		bike.soundHorn();
+		
+		Car scooty= new Car();
+		scooty.applyBreak();
+		scooty.soundHorn();
+
+	}
+
+}

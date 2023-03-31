d514da32ecb9c08c2d5baed474d85e6916d64fce
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-31 20:55:36 +0530

    Add files via upload
    Chrome.java

---
+++ Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+
+	public void getName() {
+		
+	}
+	public void PrintName() {
+		
+	}
+	public static void main(String[] args) {
+
+		System.out.println("This is google Chrome");
+	}
+
+}

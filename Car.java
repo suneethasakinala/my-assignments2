15f760b9aec48037ea181fe4309a6e829a3250ad
suneethasakinala <128903355+suneethasakinala@users.noreply.github.com>
2023-03-31 20:52:28 +0530

    Add files via upload
    car.java

---
+++ Car.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Car {
+	
+	public void applyBreak() {
+		
+	}
+	public void soundHorn() {
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("appliedBreak");
+        System.out.println("soundHorn");
+        
+        Car maruthi= new Car();
+        maruthi.applyBreak();
+        maruthi.soundHorn();
+        
+        Car skoda= new Car();
+        skoda.applyBreak();
+        skoda.soundHorn();
+	}
+	
+
+}
